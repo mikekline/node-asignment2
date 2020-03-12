e3d7182f9568a0c19f72d6145c748bee1c7d0dac
Michael Kline <klinem@gmail.com>
2020-03-11 18:42:09 -0600

    added more set up

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.set('view engine','ejs');
+
+app.get('/',function(req, res) {  
+  res.render('index');
+});
+
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//error handling
+app.use(function (req, res, next) {
+  res.status(404);
+  res.send('404: Not Found');
+});
+
+//server running on port 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
